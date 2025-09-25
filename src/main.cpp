7438cc2e1f1335eabbec7ca04a125b1c9ed1ac28
Julien Combattelli <julien.combattelli@gmail.com>
2025-09-25 17:21:04 +0200

    Add cli argument parsing to open file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,53 @@
+#include <cstdlib>
+#include <string_view>
 #include <ted/editor.hpp>
 #include <ted/os.hpp>
 #include <ted/tui.hpp>
 
 #include <cctype>
 #include <cstdio>
+#include <span>
 
-namespace ted {
+struct Arguments {
+    std::vector<std::string> files;
+};
 
-} // namespace ted
-
-int main(int /*argc*/, char* /*argv*/[])
+static void usage()
 {
+    static constexpr char usage_message[] = R"(
+Usage:
+    ted [options] [file ...]
+
+Options:
+    --      All arguments after this will be interpreted as files to open
+)";
+    // TODO add -d, --display-mode {tui,gui}
+    (void)std::fputs(usage_message, stderr);
+}
+
+static Arguments parse_arguments(std::span<char*> args)
+{
+    Arguments arguments;
+    bool swallow_remaining_as_files = false;
+    for (std::string_view arg : args.subspan(1)) {
+        if (arg.starts_with('-') && !swallow_remaining_as_files) {
+            if (arg == "--") {
+                swallow_remaining_as_files = true;
+            } else {
+                usage();
+                std::exit(EXIT_FAILURE);
+            }
+        } else {
+            arguments.files.emplace_back(arg);
+        }
+    }
+    return arguments;
+}
+
+int main(int argc, char* argv[])
+{
+    Arguments args = parse_arguments(std::span(argv, argc));
+
     if (!ted::os::isatty(stdin) || !ted::os::isatty(stdout)) {
         std::fprintf(stderr, "not a tty\n");
         return 1;
@@ -18,7 +55,12 @@ int main(int /*argc*/, char* /*argv*/[])
 
     ted::editor::init();
     ted::tui::init();
-    // TODO handle input files from argv
-    ted::editor::open_new_file();
+    if (args.files.size() == 0) {
+        ted::editor::open_new_file();
+    } else {
+        for (const auto& filepath : args.files) {
+            ted::editor::open_file(filepath.c_str());
+        }
+    }
     ted::tui::start();
 }
--- src/ted/tui.cpp
+++ src/ted/tui.cpp
@@ -239,6 +239,7 @@ static void refresh_screen()
         editor::File& file = *editor::state.viewed_file;
         for (const auto& line : file.lines) {
             editor::screen_buffer_append(line.c_str());
+            editor::screen_buffer_append("\r\n");
         }
         eob_row = file.lines.size();
     }
